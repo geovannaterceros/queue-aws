9c59f8f6305a28118f94e4a985e3c999dcd4ee88
Geovanna <geovannaterceros@gmail.com>
2023-09-22 15:46:31 -0400

    feat: add consumer for queue with mediator for delete queue.

---
+++ Customers.Consumer/Customers.Consumer/Handlers/CustomerUdpateHandler.cs
@@ -0,0 +1,20 @@
+﻿using Customers.Consumer.Messages;
+using MediatR;
+
+namespace Customers.Consumer.Handlers;
+
+public class CustomerUdpateHandler : IRequestHandler<CustomerUpdated>
+{
+    private readonly ILogger<CustomerUdpateHandler> _logger;
+
+    public CustomerUdpateHandler(ILogger<CustomerUdpateHandler> logger)
+    {
+        _logger = logger;
+    }
+    
+    public Task<Unit> Handle(CustomerUpdated request, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation(request.FullName);
+        return Unit.Task;
+    }
+}
---
+++ Customers.Consumer/Customers.Consumer/QueueSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Customers.Consumer;
+
+public class QueueSettings
+{
+    public const string key = "Queue";
+    public required string Name { get; set; }
+}
