a29b26a517a0d08f697c8ac09a8d9638212cee16
Geovanna <geovannaterceros@gmail.com>
2023-09-29 16:18:41 -0400

    feat: add for uploading data with S3

---
+++ aws-fundamentals-final-master/6.S3/S3Playground/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Amazon.S3;
+using Amazon.S3.Model;
+
+
+var s3Client = new AmazonS3Client();
+
+await using var inputstream = new FileStream("./movies.csv", FileMode.Open, FileAccess.Read);
+
+var putObjectRequest = new PutObjectRequest
+{
+    BucketName = "geoawscourse",
+    Key = "file/csv",
+    ContentType = "text/csv",
+    InputStream = inputstream
+};
+
+await s3Client.PutObjectAsync(putObjectRequest);
+
+Console.WriteLine();
