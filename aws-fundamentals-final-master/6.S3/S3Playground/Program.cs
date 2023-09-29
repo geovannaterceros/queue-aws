a35950ad7a0aff57fb9aae10d404ace157041a8c
Geovanna <geovannaterceros@gmail.com>
2023-09-29 16:27:34 -0400

    feat: download file csv and show in console.

--- aws-fundamentals-final-master/6.S3/S3Playground/Program.cs
+++ aws-fundamentals-final-master/6.S3/S3Playground/Program.cs
@@ -1,20 +1,35 @@
 ﻿
+using System.Text;
 using Amazon.S3;
 using Amazon.S3.Model;
 
 
 var s3Client = new AmazonS3Client();
 
-await using var inputstream = new FileStream("./movies.csv", FileMode.Open, FileAccess.Read);
-
-var putObjectRequest = new PutObjectRequest
+var getObjectRequest = new GetObjectRequest
 {
     BucketName = "geoawscourse",
-    Key = "file/csv",
-    ContentType = "text/csv",
-    InputStream = inputstream
+    Key = "file/csv"
 };
 
-await s3Client.PutObjectAsync(putObjectRequest);
+var response = await s3Client.GetObjectAsync(getObjectRequest);
+
+using var memoryStream = new MemoryStream();
+response.ResponseStream.CopyTo(memoryStream);
+
+var text = Encoding.Default.GetString(memoryStream.ToArray());
+
+Console.WriteLine(text);
+// await using var inputstream = new FileStream("./movies.csv", FileMode.Open, FileAccess.Read);
+//
+// var putObjectRequest = new PutObjectRequest
+// {
+//     BucketName = "geoawscourse",
+//     Key = "file/csv",
+//     ContentType = "text/csv",
+//     InputStream = inputstream
+// };
+//
+// await s3Client.PutObjectAsync(putObjectRequest);
 
 Console.WriteLine();
