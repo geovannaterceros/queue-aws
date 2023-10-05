cddc1142338a1f371eae54bd1bbd758bf7c33879
Geovanna <geovannaterceros@gmail.com>
2023-10-05 18:07:52 -0400

    fact: add secret manager api

---
+++ aws-fundamentals-final-master/7.SecretsManager/SecretManagerApp/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+var secretsManagerClient = new AmazonSecretsManagerClient();
+
+var listSecretVersionRequest = new ListSecretVersionIdsRequest
+{
+    SecretId    = "ApiKey",
+    IncludeDeprecated = true
+};
+
+var versionResponse = await secretsManagerClient.ListSecretVersionIdsAsync(listSecretVersionRequest);
+
+var request = new GetSecretValueRequest
+{
+    SecretId = "ApiKey",
+    VersionStage = "AWSCURRENT"
+};
+
+var response = await secretsManagerClient.GetSecretValueAsync(request);
+
+
+var describeSecretRequest = new DescribeSecretRequest
+{
+    SecretId = "ApiKey"
+};
+
+var describeResponse = await secretsManagerClient.DescribeSecretAsync(describeSecretRequest);
+
+Console.WriteLine(describeResponse);
+Console.WriteLine(response.SecretString);
